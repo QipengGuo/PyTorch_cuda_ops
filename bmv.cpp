40bb747c2c8ccccc7132b16be3375558260c9cbc
Ubuntu <ubuntu@ip-172-31-47-98.ec2.internal>
2019-03-27 13:00:44 +0000

    new kernel

--- bmv.cpp
+++ bmv.cpp
@@ -5,18 +5,88 @@
 #define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
-at::Tensor bmv_cuda_forward(
+at::Tensor outer_cuda_forward(
 	const at::Tensor& A,
 	const at::Tensor& B);
 
-at::Tensor bmv_forward(
+at::Tensor outer_forward(
 	const at::Tensor& A,
 	const at::Tensor& B) {
 	CHECK_INPUT(A);
 	CHECK_INPUT(B);
-	return bmv_cuda_forward(A, B);
+	return outer_cuda_forward(A, B);
+}
+at::Tensor bmv4_cuda_forward1(
+	const at::Tensor& A,
+	const at::Tensor& B0,
+	const at::Tensor& B1,
+	const at::Tensor& B2,
+	const at::Tensor& B3);
+
+at::Tensor bmv4_forward1(
+	const at::Tensor& A,
+	const at::Tensor& B0,
+	const at::Tensor& B1,
+	const at::Tensor& B2,
+	const at::Tensor& B3) {
+	CHECK_INPUT(A);
+	CHECK_INPUT(B0);
+	CHECK_INPUT(B1);
+	CHECK_INPUT(B2);
+	CHECK_INPUT(B3);
+	return bmv4_cuda_forward1(A, B0, B1, B2, B3);
 }
 
+at::Tensor bmv4_cuda_forward2(
+	const at::Tensor& A,
+	const at::Tensor& B0,
+	const at::Tensor& B1,
+	const at::Tensor& B2,
+	const at::Tensor& B3);
+
+at::Tensor bmv4_forward2(
+	const at::Tensor& A,
+	const at::Tensor& B0,
+	const at::Tensor& B1,
+	const at::Tensor& B2,
+	const at::Tensor& B3) {
+	CHECK_INPUT(A);
+	CHECK_INPUT(B0);
+	CHECK_INPUT(B1);
+	CHECK_INPUT(B2);
+	CHECK_INPUT(B3);
+	return bmv4_cuda_forward2(A, B0, B1, B2, B3);
+}
+
+at::Tensor bmv_cuda_forward2(
+	const at::Tensor& A,
+	const at::Tensor& B);
+
+at::Tensor bmv_forward2(
+	const at::Tensor& A,
+	const at::Tensor& B) {
+	CHECK_INPUT(A);
+	CHECK_INPUT(B);
+	return bmv_cuda_forward2(A, B);
+}
+
+at::Tensor bmv_cuda_forward1(
+	const at::Tensor& A,
+	const at::Tensor& B);
+
+at::Tensor bmv_forward1(
+	const at::Tensor& A,
+	const at::Tensor& B) {
+	CHECK_INPUT(A);
+	CHECK_INPUT(B);
+	return bmv_cuda_forward1(A, B);
+}
+
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("bmv_forward", &bmv_forward, "Batched Mv forward");
+    m.def("bmv_forward1", &bmv_forward1, "Batched Mv forward1");
+    m.def("bmv_forward2", &bmv_forward2, "Batched Mv forward2");
+    m.def("bmv4_forward1", &bmv4_forward1, "Batched Mv forward1 for 4 inputs");
+    m.def("bmv4_forward2", &bmv4_forward2, "Batched Mv forward2 for 4 inputs");
+    m.def("outer_forward", &outer_forward, "Outer Product forward");
 }
--- bmv_kernel.cu
+++ bmv_kernel.cu
@@ -12,77 +12,254 @@
 #include <vector>
 
 namespace {
-
-/*
- * CUDA kernel of batched matrix multiplication:
- * (b, n, m) * (b, m, p)
- */
-
-/*
+// (b, m) * [m, (b, n)] = (b, n) b,ty->block, tx->n, m==4
 template <typename scalar_t>
-__global__ void bmm_kernel(const scalar_t* __restrict__ A, const scalar_t* __restrict__ B, scalar_t* __restrict__ C, const int b, const int n, const int m, const int p) {
+__global__ void bmv4_kernel2(const scalar_t* __restrict__ A, const scalar_t* __restrict__ B0, const scalar_t* __restrict__ B1, const scalar_t* __restrict__ B2, const scalar_t* __restrict__ B3, scalar_t* __restrict__ C, const int b, const int n, const int m) {
     int i = blockIdx.x;
     int tx = threadIdx.x;
     int ty = threadIdx.y;
-    for (int x = tx; x < n; x += blockDim.x) {
-        for (int y = ty; y < p; y += blockDim.y) {
-            scalar_t sum = 0;
-            for (int k = 0; k < m; ++k) {
-                sum +=  A[((i * n) + x) * m + k] * B[((i * m) + k) * p + y];
-            }
-            C[((i * n) + x) * p + y] = sum;
-        } 
+
+    for (int j=i*blockDim.y+ty; j<b; j+= gridDim.x * blockDim.y) {
+        for (int x=tx; x<n; x+= blockDim.x) {
+            C[j*n+x] = A[j*m] * B0[j*n+x] + A[j*m+1] * B1[j*n+x] + A[j*m+2] * B2[j*n+x] + A[j*m+3] * B3[j*n+x];
+        }
     }
 }
-*/
 
-
-// (b, n, m) (b, m) where n>>m ,b-> block, tx->n
+// (b, m) * [n, (b, m)] = (b, n) b,ty->block, tx->n, n==4
 template <typename scalar_t>
-__global__ void bmv_kernel(const scalar_t* __restrict__ A, const scalar_t* __restrict__ B, scalar_t* __restrict__ C, const int n, const int m) {
-//    __shared__ scalar_t la[5];
+__global__ void bmv4_kernel1(const scalar_t* __restrict__ A, const scalar_t* __restrict__ B0, const scalar_t* __restrict__ B1, const scalar_t* __restrict__ B2, const scalar_t* __restrict__ B3, scalar_t* __restrict__ C, const int b, const int n, const int m) {
     int i = blockIdx.x;
     int tx = threadIdx.x;
-//    if (tx<m) la[tx] = B[i*m+tx];
-//    __syncthreads();
+    int ty = threadIdx.y;
 
-    int i1 = i*n;
-    for (int x=tx; x<n; x+= blockDim.x) {
-//        scalar_t sum = 0;
-        int i2 = (i1+x)*m;
-        int i3 = i*m;
-//        for (int y=0; y<m; y++) {
-//            sum += A[(i*n+x)*m+y] * la[y];
-//            sum += A[i2+y] * B[i3+y];
-//        }
-        C[i1+x] = A[i2] * B[i3] + A[i2+1] * B[i3+1] + A[i2+2] * B[i3+2] + A[i2+3] * B[i3+3] + A[i2+4] * B[i3+4];
-//        C[i1+x] = sum;
+    for (int j=i*blockDim.y+ty; j<b; j+= gridDim.x * blockDim.y) {
+        scalar_t sum = 0;
+        if (tx==0) {
+            for (int y=0; y<m; y++) {
+                sum += B0[j*m+y] * A[j*m+y];
+            }
+        }
+        if (tx==1) {
+            for (int y=0; y<m; y++) {
+                sum += B1[j*m+y] * A[j*m+y];
+            }
+        }
+        if (tx==2) {
+            for (int y=0; y<m; y++) {
+                sum += B2[j*m+y] * A[j*m+y];
+            }
+        }
+        if (tx==3) {
+            for (int y=0; y<m; y++) {
+                sum += B3[j*m+y] * A[j*m+y];
+            }
+        }
+        C[j*n+tx] = sum;
+    }
+}
+
+// (b, n, 1) * (b, 1, m) = (b, n, m) n>m
+template <typename scalar_t>
+__global__ void outer_kernel(const scalar_t* __restrict__ A, const scalar_t* __restrict__ B, scalar_t* __restrict__ C, const int b, const int n, const int m) {
+    int i = blockIdx.x;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    for (int j=i; j<b; j+= gridDim.x) {
+        for (int x=tx; x<n; x+= blockDim.x){
+            for (int y=ty; y<m; y+= blockDim.y) {
+                C[(j*n+x)*m+y] = A[j*n+x] * B[j*m+y];
+            }
+        }
+    }
+}
+
+// (b, m) * (b, m, n) = (b, n) b,ty-> block, tx->n
+template <typename scalar_t>
+__global__ void bmv_kernel1(const scalar_t* __restrict__ A, const scalar_t* __restrict__ B, scalar_t* __restrict__ C, const int b, const int n, const int m) {
+    int i = blockIdx.x;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    for (int j=i*blockDim.y+ty; j<b; j+= gridDim.x * blockDim.y) {
+        for (int x=tx; x<n; x+= blockDim.x) {
+            scalar_t sum = 0;
+            for (int y=0; y<m; y++) {
+                if (y<m) sum += B[(j*m+y)*n+x] * A[j*m+y];
+            }
+            C[j*n+x] = sum;
+        }
+    }
+}
+
+// (b, m) * (b, n, m) = (b, n) b,ty-> block, tx->n
+template <typename scalar_t>
+__global__ void bmv_kernel2(const scalar_t* __restrict__ A, const scalar_t* __restrict__ B, scalar_t* __restrict__ C, const int b, const int n, const int m) {
+    int i = blockIdx.x;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    for (int j=i*blockDim.y+ty; j<b; j+= gridDim.x * blockDim.y) {
+        int i1 = j*n;
+        int i3 = j*m;
+        for (int x=tx; x<n; x+= blockDim.x) {
+            scalar_t sum = 0;
+            int i2 = (i1+x)*m;
+            for (int y=0; y<m; y++) {
+                if (y<m) sum += B[i2+y] * A[i3+y];
+            }
+            C[i1+x] = sum;
+        }
     }
 }
 
 } // End of namespace
 
-at::Tensor bmv_cuda_forward(
+at::Tensor outer_cuda_forward(
     const at::Tensor& A,
     const at::Tensor& B) {
-    // A: (b, n, m), B: (b, m)
-    const auto b = A.size(0);
+    // A: (b, n), B: (b, m)
+    const auto b = B.size(0);
     const auto n = A.size(1);
-    const auto m = A.size(2);
-    assert(m == B.size(1));
+    const auto m = B.size(1);
 
-    auto y = at::zeros({b, n}, A.options());
+    auto y = at::zeros({b, n, m}, A.options());
 
-    const dim3 threads(n);
-    const dim3 blocks(b);
+    //const dim3 threads((n<(1024/m-1))?n:(1024/m-1) , m);
+    const dim3 threads(8, m);
+//    const dim3 blocks(b);
+    const dim3 blocks(2048);
     cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-    AT_DISPATCH_FLOATING_TYPES(A.type(), "bmv_cuda_forward1", ([&] {
-        bmv_kernel<scalar_t><<<blocks, threads, 0, stream>>>(
+    AT_DISPATCH_FLOATING_TYPES(A.type(), "outer_cuda_forward", ([&] {
+        outer_kernel<scalar_t><<<blocks, threads, 0, stream>>>(
             A.data<scalar_t>(),
             B.data<scalar_t>(),
             y.data<scalar_t>(),
-            n, m);
+            b, n, m);
+    }));
+    THCudaCheck(cudaGetLastError());
+
+    return y;
+}
+
+at::Tensor bmv4_cuda_forward2(
+    const at::Tensor& A,
+    const at::Tensor& B0,
+    const at::Tensor& B1,
+    const at::Tensor& B2,
+    const at::Tensor& B3) {
+    // A: (b, m), B: [m, (b, n)]
+    const auto b = A.size(0);
+    const auto n = B0.size(1);
+    const auto m = 4;
+
+    auto y = at::zeros({b, n}, A.options());
+
+    const dim3 threads(n, (n>128)?1:8);
+//    const dim3 blocks(b);
+    const dim3 blocks(2048);
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+    AT_DISPATCH_FLOATING_TYPES(A.type(), "bmv4_cuda_forward2", ([&] {
+        bmv4_kernel2<scalar_t><<<blocks, threads, 0, stream>>>(
+            A.data<scalar_t>(),
+            B0.data<scalar_t>(),
+            B1.data<scalar_t>(),
+            B2.data<scalar_t>(),
+            B3.data<scalar_t>(),
+            y.data<scalar_t>(),
+            b, n, m);
+    }));
+    THCudaCheck(cudaGetLastError());
+
+    return y;
+}
+
+at::Tensor bmv4_cuda_forward1(
+    const at::Tensor& A,
+    const at::Tensor& B0,
+    const at::Tensor& B1,
+    const at::Tensor& B2,
+    const at::Tensor& B3) {
+    // A: (b, m), B: [n, (b, m)]
+    const auto b = A.size(0);
+    const auto n = 4;
+    const auto m = A.size(1);
+
+    auto y = at::zeros({b, n}, A.options());
+
+    const dim3 threads(n, (n>128)?1:8);
+//    const dim3 blocks(b);
+    const dim3 blocks(2048);
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+    AT_DISPATCH_FLOATING_TYPES(A.type(), "bmv4_cuda_forward1", ([&] {
+        bmv4_kernel1<scalar_t><<<blocks, threads, 0, stream>>>(
+            A.data<scalar_t>(),
+            B0.data<scalar_t>(),
+            B1.data<scalar_t>(),
+            B2.data<scalar_t>(),
+            B3.data<scalar_t>(),
+            y.data<scalar_t>(),
+            b, n, m);
+    }));
+    THCudaCheck(cudaGetLastError());
+
+    return y;
+}
+
+at::Tensor bmv_cuda_forward2(
+    const at::Tensor& A,
+    const at::Tensor& B) {
+    // A: (b, m), B: (b, n, m)
+    const auto b = B.size(0);
+    const auto n = B.size(1);
+    const auto m = B.size(2);
+    assert(m == A.size(1));
+
+    auto y = at::zeros({b, n}, A.options());
+
+    const dim3 threads(n, (n>128)?1:8);
+//    const dim3 blocks(b);
+    const dim3 blocks(2048);
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+    AT_DISPATCH_FLOATING_TYPES(A.type(), "bmv_cuda_forward2", ([&] {
+        bmv_kernel2<scalar_t><<<blocks, threads, 0, stream>>>(
+            A.data<scalar_t>(),
+            B.data<scalar_t>(),
+            y.data<scalar_t>(),
+            b, n, m);
+    }));
+    THCudaCheck(cudaGetLastError());
+
+    return y;
+}
+
+at::Tensor bmv_cuda_forward1(
+    const at::Tensor& A,
+    const at::Tensor& B) {
+    // A: (b, m), B: (b, m, n)
+    const auto b = B.size(0);
+    const auto m = B.size(1);
+    const auto n = B.size(2);
+    assert(m == A.size(1));
+
+    auto y = at::zeros({b, n}, A.options());
+
+    const dim3 threads(n, (n>128)?1:8);
+//    const dim3 blocks(b);
+    const dim3 blocks(2048);
+    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+    AT_DISPATCH_FLOATING_TYPES(A.type(), "bmv_cuda_forward1", ([&] {
+        bmv_kernel1<scalar_t><<<blocks, threads, 0, stream>>>(
+            A.data<scalar_t>(),
+            B.data<scalar_t>(),
+            y.data<scalar_t>(),
+            b, n, m);
     }));
     THCudaCheck(cudaGetLastError());
 
